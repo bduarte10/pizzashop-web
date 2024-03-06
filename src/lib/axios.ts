a6939f8db17460104725401c2fe5dcdc272cdad8
Bruno <duartebruno2208@gmail.com>
2024-03-06 00:13:57 -0300

    feat: axios instance

---
+++ src/lib/axios.ts
@@ -0,0 +1,14 @@
+import { env } from "@/env";
+import axios from "axios";
+
+export const api = axios.create({
+  baseURL: env.VITE_API_URL,
+  withCredentials: true,
+});
+
+if (env.VITE_ENABLE_API_DELAY) {
+  api.interceptors.request.use(async (config) => {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    return config;
+  });
+}
