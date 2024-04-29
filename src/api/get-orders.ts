dc2682b8fd5b8d5a273aa46200d05f56069888a0
Bruno <duartebruno2208@gmail.com>
2024-04-29 01:41:41 -0300

    fix: .tsx to .ts functions

---
+++ src/api/get-orders.ts
@@ -0,0 +1,25 @@
+import { api } from "@/lib/axios";
+
+interface GetOrdersResponse {
+  orders: {
+    orderId: string;
+    createdAt: Date;
+    status: "pending" | "canceled" | "processing" | "delivering" | "delivered";
+    customerName: string;
+    total: number;
+  }[];
+  meta: {
+    pageIndex: number;
+    perPage: number;
+    totalCount: number;
+  };
+}
+
+export async function GetOrders() {
+  const response = await api.get<GetOrdersResponse>("/orders", {
+    params: {
+      pageIndex: 0,
+    },
+  });
+  return response.data;
+}
