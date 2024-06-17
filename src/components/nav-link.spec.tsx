9dd603cbc3a5e16c4910244c150d0ed8239109cf
Bruno Duarte <duartebruno2208@gmail.com>
2024-06-17 19:46:01 -0300

    feat: nav links unitary tests

--- src/components/nav-link.spec.tsx
+++ src/components/nav-link.spec.tsx
@@ -4,12 +4,21 @@ import { MemoryRouter } from "react-router-dom";
 
 describe("NavLink", () => {
   it("should highlight the active link", () => {
-    const wrapper = render(<NavLink to="/about" />, {
-      wrapper: ({ children }) => {
-        return (
-          <MemoryRouter initialEntries={["/about"]}>{children}</MemoryRouter>
-        );
+    const wrapper = render(
+      <>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/about">About</NavLink>
+      </>,
+      {
+        wrapper: ({ children }) => {
+          return (
+            <MemoryRouter initialEntries={["/about"]}>{children}</MemoryRouter>
+          );
+        },
       },
-    });
+    );
+
+    expect(wrapper.getByText("Home").dataset.current).toEqual("false");
+    expect(wrapper.getByText("About").dataset.current).toEqual("true");
   });
 });
